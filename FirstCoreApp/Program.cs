7b8189e82d9dc1294a90efc5e30ed711ad632955
Saikat Ghosh <>
2023-11-14 18:41:55 +0530

    First Commit

--- FirstCoreApp/Program.cs
+++ FirstCoreApp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "My first >net Core Programe in VSCode!");
 
 app.Run();
